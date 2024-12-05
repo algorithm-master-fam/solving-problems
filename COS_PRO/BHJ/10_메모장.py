1803ec3eb06bdd242c5ba54af85b9d47bb8b7a0c
heejinssss <heejin.dev33@gmail.com>
2024-12-05 21:22:22 +0900

    BHJ_COS_PRO

--- COS_PRO/BHJ/10_메모장.py
+++ COS_PRO/BHJ/10_메모장.py
@@ -1,25 +1,18 @@
-# 풀이 중
-
 # 다음과 같이 import를 사용할 수 있습니다.
 # import math
 
 def solution(K, words):
     # 여기에 코드를 작성해주세요.
     answer = 1
-    lk = K
+    cur = 0
 
-    for word in words:
-        word_length = len(word)
-        # 현재 줄에 단어를 추가할 수 없는 경우
-        if lk < word_length + (1 if lk < K else 0):
-            answer += 1  # 새로운 줄 시작
-            lk = K  # 남은 공간 초기화
-
-        # 단어를 현재 줄에 추가
-        lk -= word_length
-        if lk > 0:  # 공백이 필요한 경우
-            lk -= 1
-
+    for i in range(len(words)):
+        if cur + len(words[i]) <= K:
+            cur += len(words[i]) + 1 # 글자 카운트 누적
+        else:
+            cur = len(words[i]) + 1 # 다시 이 글자부터 카운트 시작
+            answer += 1
+    
     return answer
         
 # 아래는 테스트케이스 출력을 해보기 위한 코드입니다.
